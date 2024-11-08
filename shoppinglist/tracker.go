34e5a21e96049ccde5e43732e80abb0804a0dce8
tgt <kasyap3103@gmail.com>
2024-11-08 16:56:22 +0530

    fix: item-service

--- shoppinglist/tracker.go
+++ shoppinglist/tracker.go
@@ -1,22 +1,25 @@
 package shoppinglist
 
-type Item struct {
-	ID int  `json:"id"`
-	Name string `json:"name"`
-	Price float64 `json:"price"`
-	Quantity int `json:"quantity"`
-	Bought bool `json:"bought"`
+import "context"
 
+type Item struct {
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	Quantity int     `json:"quantity"`
+	Bought   bool    `json:"bought"`
+}
+type getItemsResponse struct {
+	Items []Item `json:"items"`
 }
 
-//encore:api public method=GET path=/items 
-func (s *Service)GetItems()(*[]Item,error){
+//encore:api public method=GET path=/items
+func (s *Service) GetItems(context.Context) (*getItemsResponse, error) {
 	var items []Item
 	err := s.db.Find(&items).Error
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return &items,nil
+	return &getItemsResponse{Items: items}, nil
 
 }
-
